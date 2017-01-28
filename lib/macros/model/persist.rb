80e7d64a34b6b444041d70219511f54c95856552
Maciej Mensfeld <maciej@mensfeld.pl>
2017-01-28 20:07:09 +0100

    more specs

--- lib/macros/model/persist.rb
+++ lib/macros/model/persist.rb
@@ -5,11 +5,9 @@ module Macros
     # @example
     #   step Macros::Model::Persist()
     class Persist < Base
-      # @param name [String] options key name under which is object that we want to persist
       # @param method [Symbol] name of a method that we want to use to save model
       # @return [Macros::Model::Persist] persist step instance with appropriate options
-      def initialize(name: 'model', method: :save!)
-        @name = name
+      def initialize(method: :save!)
         @method = method
       end
 
@@ -17,7 +15,7 @@ module Macros
       # @param options [Trailblazer::Operation::Option] options accumulator
       # @param model [Object] object that we want to persist
       def call(options, model:, **)
-        options[@name].public_send(@method)
+        model.public_send(@method)
       end
     end
   end
---
+++ spec/lib/macros/model/persist_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+RSpec.describe Macros::Model::Persist do
+  let(:scope) do
+    Class.new do
+      def save!; end
+      def save; end
+
+      self
+    end
+  end
+
+  let(:model) { instance_double(scope) }
+
+  describe 'default save!' do
+    subject(:persist_step) { described_class.new }
+
+    it 'expect to save model' do
+      expect(model).to receive(:save!)
+      persist_step.call({}, model: model)
+    end
+  end
+
+  describe 'different save method' do
+    subject(:persist_step) { described_class.new(method: :save) }
+
+    it 'expect to save model' do
+      expect(model).to receive(:save)
+      persist_step.call({}, model: model)
+    end
+  end
+end
