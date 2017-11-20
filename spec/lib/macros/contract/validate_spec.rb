abe3ff6cdcb55c60d93a87849349c723a61e3fbb
Maciej Mensfeld <maciej@mensfeld.pl>
2017-11-20 15:50:53 +0100

    default validation key set to data

--- spec/lib/macros/contract/validate_spec.rb
+++ spec/lib/macros/contract/validate_spec.rb
@@ -3,12 +3,38 @@
 RSpec.describe Macros::Contract::Validate do
   subject(:validate_step) { described_class.new(*args) }
 
-  let(:args) { { rand => rand } }
+  context 'when non argument one' do
+    let(:args) { [] }
 
-  it 'expect to delegate to trbr contract build' do
-    expect(Trailblazer::Operation::Contract).to receive(:Validate)
-      .with(*args)
+    it 'expect to delegate to trbr contract build' do
+      expect(Trailblazer::Operation::Contract).to receive(:Validate)
+        .with(key: :data)
 
-    validate_step.call
+      validate_step.call
+    end
+  end
+
+  context 'when argument one' do
+    context 'when without a key' do
+      let(:args) { [{ rand => rand }] }
+
+      it 'expect to delegate to trbr contract build' do
+        expect(Trailblazer::Operation::Contract).to receive(:Validate)
+          .with(args.last.merge(key: :data))
+
+        validate_step.call
+      end
+    end
+
+    context 'when with a key' do
+      let(:args) { [{ key: rand }] }
+
+      it 'expect to delegate to trbr contract build' do
+        expect(Trailblazer::Operation::Contract).to receive(:Validate)
+          .with(*args)
+
+        validate_step.call
+      end
+    end
   end
 end
