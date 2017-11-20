f63236ec92e24eb981a9579fa7f95270caa2d97b
Maciej Mensfeld <maciej@mensfeld.pl>
2017-11-20 15:59:03 +0100

    default key as a string

--- lib/macros/contract/validate.rb
+++ lib/macros/contract/validate.rb
@@ -8,7 +8,7 @@ module Macros
     #   step Macros::Contract::Validate(key: :repository)
     class Validate < Base
       # Name of a default validation key
-      DEFAULT_KEY = :data
+      DEFAULT_KEY = 'data'
 
       # Creates a contact validation step
       def call
