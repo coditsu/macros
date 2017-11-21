9e57601824b0b6bb0febafc3b5dd69b68579d8ce
Maciej Mensfeld <maciej@mensfeld.pl>
2017-11-21 08:53:55 +0100

    revert

--- lib/macros/contract/validate.rb
+++ lib/macros/contract/validate.rb
@@ -7,17 +7,8 @@ module Macros
     # @example
     #   step Macros::Contract::Validate(key: :repository)
     class Validate < Base
-      # Name of a default validation key
-      DEFAULT_KEY = :data
-
       # Creates a contact validation step
       def call
-        if args.last.is_a?(Hash)
-          args.last[:key] ||= DEFAULT_KEY
-        else
-          args << { key: DEFAULT_KEY }
-        end
-
         Trailblazer::Operation::Contract::Validate(*args)
       end
     end
--- spec/lib/macros/contract/validate_spec.rb
+++ spec/lib/macros/contract/validate_spec.rb
@@ -3,38 +3,12 @@
 RSpec.describe Macros::Contract::Validate do
   subject(:validate_step) { described_class.new(*args) }
 
-  context 'when non argument one' do
-    let(:args) { [] }
+  let(:args) { { rand => rand } }
 
-    it 'expect to delegate to trbr contract build' do
-      expect(Trailblazer::Operation::Contract).to receive(:Validate)
-        .with(key: :data)
+  it 'expect to delegate to trbr contract build' do
+    expect(Trailblazer::Operation::Contract).to receive(:Validate)
+      .with(*args)
 
-      validate_step.call
-    end
-  end
-
-  context 'when argument one' do
-    context 'when without a key' do
-      let(:args) { [{ rand => rand }] }
-
-      it 'expect to delegate to trbr contract build' do
-        expect(Trailblazer::Operation::Contract).to receive(:Validate)
-          .with(args.last.merge(key: :data))
-
-        validate_step.call
-      end
-    end
-
-    context 'when with a key' do
-      let(:args) { [{ key: rand }] }
-
-      it 'expect to delegate to trbr contract build' do
-        expect(Trailblazer::Operation::Contract).to receive(:Validate)
-          .with(*args)
-
-        validate_step.call
-      end
-    end
+    validate_step.call
   end
 end
