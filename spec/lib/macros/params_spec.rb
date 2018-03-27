c4e2ffdf0b6b2ba3e2b420d03d764987ed27ee10
Maciej Mensfeld <maciej@mensfeld.pl>
2018-03-27 11:51:17 +0200

    remove unused macro for remap

--- spec/lib/macros/params_spec.rb
+++ spec/lib/macros/params_spec.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
 RSpec.describe Macros::Params do
-  describe '#Remap()' do
-    let(:remapper_class) { Class.new }
-
-    it { expect(described_class::Remap(remapper_class)).to be_a described_class::Remap }
-  end
+  pending
 end
