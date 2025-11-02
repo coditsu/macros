af8e2ec54ad140ce45cda7c9c1662440ab6f40a8
Tomasz Pajor <tomek@polishgeeks.com>
2025-11-02 21:59:16 +0100

    more

--- spec/lib/macros/error_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe_current do
-  describe '#Raise()' do
-    it { expect(described_class::Raise.new(Class.new)).to be_a described_class::Raise }
-  end
-end
--- spec/lib/macros/karafka_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe_current do
-  let(:scope) { Class.new }
-
-  describe '#Broadcast()' do
-    it { expect(described_class::Broadcast.new(scope)).to be_a described_class::Broadcast }
-  end
-end
